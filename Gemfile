32a9049d6d4e79360d7b24939075d2e268ff2f97
Steve Ellis <email@steveell.is>
2013-11-10 14:28:32 -0500

    Use Haml

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'haml'
 gem 'jquery-rails'
 gem 'pg'
 gem 'rails', '4.0.1'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ PublicStand::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.generators do |generator|
+    generator.template_engine :haml
+  end
 end
