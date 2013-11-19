1c5676d52f4231e4b645ee52158ec8af7a881918
Steve Ellis <email@steveell.is>
2013-11-19 10:52:18 -0500

    Capture user test, add back later

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,19 @@
+require 'feature_helper'
+
+describe "Signing up" do
+  xit "allows the user to create a new login" do
+    visit new_authentication_path
+
+    fill_in "Email", with: 'zbarnacles@slugline.com'
+    fill_in "Password", with: 'frankyPanky'
+    fill_in "Password Confirmation", with: 'frankyPanky'
+    click_button 'Sign Up'
+    page.should have_content "Congratulations! You're awesome!"
+
+    fill_in "Name", with: 'Zoe Barnes'
+    fill_in "Phone number", with: '(518)334-6656'
+    fill_in "Zip code", with: '11211'
+    click_button 'Create User'
+    page.should have_content "Welcome Zoe Barnes!"
+  end
+end
