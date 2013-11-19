3a675fc6207e9fc4ae851e04450edb5760408543
Steve Ellis <email@steveell.is>
2013-11-19 10:13:42 -0500

    Organizations must have a name to be valid

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,6 @@
 class Organization < ActiveRecord::Base
   has_many :memberships
   has_many :members, through: :memberships
+
+  validates :name, presence: true
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
 describe Organization do
+  describe "validations" do
+    it { should have_valid(:name).when('Clear Water Initiative') }
+    it { should_not have_valid(:name).when(nil, '') }
+  end
 end
