cfb6f2caefbe1d2d7dab2d289ec6eedfb4835844
Steve Ellis <email@steveell.is>
2013-12-15 14:58:27 -0500

    Use AJAX to create Twilio tokens

--- app/assets/javascripts/callTrigger.js
+++ app/assets/javascripts/callTrigger.js
@@ -1,9 +1,16 @@
 callTrigger = function callTrigger(element) {
   var $element = $(element);
 
-  Twilio.Device.setup($element.data('token'));
-
   $element.click(function () {
-    Twilio.Device.connect()
+    var url = '/campaigns/' + $element.data('campaign-id') + '/phone_calls';
+    $.ajax({
+      type: 'POST',
+      url: url,
+      beforeSend: function (xhr) { xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content')) },
+      success: function (data, textStatus, jqXHR) {
+        Twilio.Device.setup(data.twilio_token);
+        Twilio.Device.connect();
+      }
+    });
   });
 }
