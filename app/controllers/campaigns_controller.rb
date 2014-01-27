6525911ffae4f56ba30db6e9bd5b8d85c37ea891
Steve Ellis <email@steveell.is>
2014-01-27 17:30:09 -0500

    Clean up CampaignsController

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,22 +2,15 @@ class CampaignsController < ApplicationController
   load_and_authorize_resource through: :organization
 
   def new
-    @organization = Organization.find(params[:organization_id])
-    @campaign = Campaign.new
     build_campaign_target
   end
 
   def edit
-    @organization = Organization.find(params[:organization_id])
-    @campaign = @organization.campaigns.find(params[:id])
     @campaign_target = @campaign.campaign_targets.first
     @target = @campaign_target.target
   end
 
   def create
-    @organization = Organization.find(params[:organization_id])
-    @campaign = @organization.campaigns.build(Params.clean params)
-
     if @campaign.save
       flash.notice = 'Successfully created a new campaign'
       redirect_to [@organization, @campaign]
@@ -29,9 +22,6 @@ class CampaignsController < ApplicationController
   end
 
   def update
-    @organization = Organization.find(params[:organization_id])
-    @campaign = @organization.campaigns.find(params[:id])
-
     if @campaign.update_attributes(Params.clean(params))
       flash.notice = 'Campaign successfully updated'
       redirect_to [@organization, @campaign]
@@ -44,8 +34,6 @@ class CampaignsController < ApplicationController
   end
 
   def show
-    @organization = Organization.find(params[:organization_id])
-    @campaign = @organization.campaigns.find(params[:id])
     @twilio_token = TwilioClient.outgoing_token
   end
 
@@ -53,7 +41,7 @@ class CampaignsController < ApplicationController
   private
 
   def organization
-    Organization.find(params[:organization_id])
+    @organization ||= Organization.find(params[:organization_id])
   end
 
   def build_campaign_target
