aba1e9e06a3de84a1fd625e11cb8496109b91207
Steve Ellis <email@steveell.is>
2014-02-03 12:10:22 -0500

    Create edit page for pictures

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -1,8 +1,36 @@
 require 'spec_helper'
 
 describe PicturesController do
-  describe "s3_update" do
-    let(:picture) { FactoryGirl.create(:picture) }
+  describe "#edit" do
+    let(:user) { users(:gillian)  }
+    let(:campaign) { user.organizations.first.campaigns.first }
+    let(:picture) { FactoryGirl.create(:picture, campaign: campaign) }
+
+    before { log_in user }
+
+    it "sets up an uploader" do
+      get :edit, id: picture.id
+
+      expect(assigns :picture_uploader).to be_a PictureUploader
+
+      expect(assigns :campaign).to eq(campaign)
+      expect(assigns :organization).to eq(campaign.organization)
+    end
+  end
+
+  describe "#s3_update" do
+    # example params:
+    # {
+      # "bucket"=>"publicstand-development",
+      # "key"=>"uploads/uploader/1/3b16afc2-382b-4c52-8fdf-c0e1f88724ef/Screen Shot 2013-12-30 at 4.20.18 PM.png",
+      # "etag"=>"\"5a4ad7510d533f9ff17ce98f50137451\"",
+      # "action"=>"s3_update",
+      # "controller"=>"pictures",
+      # "picture_id"=>"1"
+    # }
+    let(:user) { users(:gillian)  }
+    let(:campaign) { user.organizations.first.campaigns.first }
+    let(:picture) { FactoryGirl.create(:picture, campaign: campaign) }
     let(:params) do
       {
         bucket: "publicstand-development",
@@ -11,6 +39,8 @@ describe PicturesController do
         picture_id: picture.id
       }
     end
+
+    before { log_in user }
 
     it "updates the pictures's remote url" do
       expect {
@@ -24,13 +54,3 @@ describe PicturesController do
     end
   end
 end
-
-# example params:
-# {
-  # "bucket"=>"publicstand-development",
-  # "key"=>"uploads/uploader/1/3b16afc2-382b-4c52-8fdf-c0e1f88724ef/Screen Shot 2013-12-30 at 4.20.18 PM.png",
-  # "etag"=>"\"5a4ad7510d533f9ff17ce98f50137451\"",
-  # "action"=>"s3_update",
-  # "controller"=>"pictures",
-  # "picture_id"=>"1"
-# }
