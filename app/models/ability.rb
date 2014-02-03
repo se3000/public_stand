7dc309fcead0a320c2f47cc86c5a3e9b138531f7
Steve Ellis <email@steveell.is>
2014-02-03 12:09:08 -0500

    Update ability for pictures

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,9 @@ class Ability
 
       can :create, Organization
       can :manage, Organization, id: user.organization_ids
+
+      can :edit, Picture, campaign_id: user.organizations.map(&:campaign_ids).flatten
+      can :s3_update, Picture, campaign_id: user.organizations.map(&:campaign_ids).flatten
     end
     can :read, Campaign
     can :read, Organization
