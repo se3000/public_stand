e6a27b397109874d8ccd9f4a2b69d716c8987a7d
Steve Ellis <email@steveell.is>
2014-02-03 11:26:53 -0500

    Pictures know where they are hosted

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,6 +3,11 @@ class Picture < ActiveRecord::Base
 
   belongs_to :campaign
 
+  def url
+    return unless s3_key.present?
+    "http://s3-us-west-2.amazonaws.com/#{ENV['AWS_BUCKET']}/#{s3_key}"
+  end
+
   def uploader_changed?
     false
   end
