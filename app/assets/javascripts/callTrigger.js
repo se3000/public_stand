d18662f69655138c20efc7fa32288b8fb9e61ff7
Steve Ellis <email@steveell.is>
2014-02-07 15:02:24 -0500

    Prompt user to enable microphone before the call starts

--- app/assets/javascripts/callTrigger.js
+++ app/assets/javascripts/callTrigger.js
@@ -24,6 +24,12 @@ PublicStand = {
       },
       success: function (data, textStatus, jqXHR) {
         Twilio.Device.setup(data.twilio_token);
+        Twilio.Device.connect(function () {
+          var $instruction = $('#instruction');
+          $('.step-1').hide();
+          $('.step-2').show();
+          $('.hang-up-btn').removeClass('disabled');
+        });
         Twilio.Device.connect({phone_call_id: data.phone_call_id});
       }
     });
