8f92199c29df17886931d80b4c1f56453efb50b8
Eirik Dentz Sinclair and Steve Ellis <pair+eirik+steve@blinkbuggy.com>
2014-02-10 03:31:27 -0500

    Add a Facebook share button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def facebook_share_button(url)
+    <<-HTML.html_safe
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=591276310956138";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <div class="fb-share-button" data-href="#{url}" data-width="1000" data-type="button"></div>
+    HTML
+  end
 end
