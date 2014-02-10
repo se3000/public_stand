884ee7bda9dcd585275adfa800111ba59b02c4d0
Steve Ellis <email@steveell.is>
2014-02-10 15:13:24 -0500

    Create an #average_call_time method

--- app/models/campaign_target.rb
+++ app/models/campaign_target.rb
@@ -5,4 +5,18 @@ class CampaignTarget < ActiveRecord::Base
   validates :campaign, :target, presence: true
 
   accepts_nested_attributes_for :target
+
+  def average_call_time
+    return unless phone_calls.any?
+    average = phone_calls.where(status: 'completed').average(:call_duration)
+    minutes = average.to_i / 60
+    seconds = (average % 60).to_i
+    "#{minutes}:#{seconds}"
+  end
+
+  private
+
+  def phone_calls
+    PhoneCall.where(campaign_id: campaign_id, target_id: target_id)
+  end
 end
--- spec/models/campaign_target_spec.rb
+++ spec/models/campaign_target_spec.rb
@@ -8,4 +8,23 @@ describe CampaignTarget do
     it { should have_valid(:target).when(Target.all.sample) }
     it { should_not have_valid(:target).when(nil) }
   end
+
+  describe "#average_call_time" do
+    subject(:campaign_target) { campaign_targets(:claire_campaign_target) }
+
+    context "when there are phone calls" do
+      before do
+        FactoryGirl.create(:phone_call, call_duration: 120, status: 'completed', campaign: campaign_target.campaign, target: campaign_target.target)
+        FactoryGirl.create(:phone_call, call_duration: 90, status: 'completed', campaign: campaign_target.campaign, target: campaign_target.target)
+      end
+
+      its(:average_call_time) { should == '1:45' }
+    end
+
+    context "when there are no phone calls" do
+      before { PhoneCall.where(campaign: campaign_target.campaign, target: campaign_target.target).destroy_all }
+
+      its(:average_call_time) { should be_nil }
+    end
+  end
 end
