f770127cce617a0ddf4c6a4037965958c347e047
Steve Danger Ellis <email@steveell.is>
2014-02-13 16:17:07 -0500

    Extract call button helper

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -2,4 +2,15 @@ module CampaignsHelper
   def average_call_time_of(cmpgn_trgt)
     cmpgn_trgt.average_call_time || "0:00 (No calls completed yet)"
   end
+
+  def in_browser_call_button(campaign_target, options = {})
+    link_to("Call #{campaign_target.target.name} from your computer", '#', {
+      class: 'btn',
+      data: {
+        behavior: 'callTrigger',
+        campaign_id: campaign_target.campaign_id,
+        auto_trigger: options[:auto_trigger]
+      }
+    })
+  end
 end
