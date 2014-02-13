f401970717a2ee890b318b428192d786c0ae965f
Steve Danger Ellis <email@steveell.is>
2014-02-13 16:21:08 -0500

    Don't use deprecated syntax

--- app/assets/javascripts/callTrigger.js
+++ app/assets/javascripts/callTrigger.js
@@ -2,14 +2,16 @@ callTrigger = function callTrigger(element) {
   var $element = $(element);
   var campaignID = $element.data('campaign-id');
 
-  function startCall() {
-    PublicStand.displayInstructions();
+  function startCall(event) {
     PublicStand.callCampaign(campaignID);
+    PublicStand.displayInstructions();
     $element.hide();
-    return false;
   }
 
-  $element.click(startCall)
+  $element.click(function (event) {
+    startCall()
+    event.preventDefault();
+  });
 
   if($element.data('auto-trigger')) {
     startCall();
