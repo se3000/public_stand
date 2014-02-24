92f8953909bd9f7454f1884fda924b2dfe525567
Steve Ellis <email@steveell.is>
2014-02-24 14:41:10 -0500

    Social links open in a new window

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def facebook_share_button(url)
-    link_to image_tag('facebook_share.png', style: 'width: 110px; height: 30px;'), "https://www.facebook.com/sharer/sharer.php?u=#{cgi_escape(url)}"
+    link_to image_tag('facebook_share.png', style: 'width: 110px; height: 30px;'), "https://www.facebook.com/sharer/sharer.php?u=#{cgi_escape(url)}", target: '_blank'
   end
 
   def tweet_button(url, description)
-    link_to image_tag('twitter_share.png', style: 'width: 110px; height: 30px;'), "https://twitter.com/intent/tweet?related=publicstand&source=tweetbutton&text=#{description}: #{cgi_escape(url)}"
+    link_to image_tag('twitter_share.png', style: 'width: 110px; height: 30px;'), "https://twitter.com/intent/tweet?related=publicstand&source=tweetbutton&text=#{description}: #{cgi_escape(url)}", target: '_blank'
   end
 
   def cgi_escape(url)
