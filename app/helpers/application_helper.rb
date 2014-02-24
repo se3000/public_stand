ff6fe63ef86bc2cc946b6def310c7a3567a77772
Steve Ellis <email@steveell.is>
2014-02-24 00:26:04 -0500

    Fixup social share images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,13 @@
 module ApplicationHelper
   def facebook_share_button(url)
-    <<-HTML.html_safe
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=591276310956138";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-
-    <div class="fb-share-button" data-href="#{url}" data-width="1000" data-type="button"></div>
-    HTML
+    link_to image_tag('facebook_share.png', style: 'width: 110px; height: 30px;'), "https://www.facebook.com/sharer/sharer.php?u=#{cgi_escape(url)}"
   end
 
   def tweet_button(url, description)
-    <<-HTML.html_safe
-      <a href="https://twitter.com/share" class="twitter-share-button" data-url="#{url}" data-text="#{description}" data-count="none">Tweet</a>
-      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-    HTML
+    link_to image_tag('twitter_share.png', style: 'width: 110px; height: 30px;'), "https://twitter.com/intent/tweet?related=publicstand&source=tweetbutton&text=#{description}: #{cgi_escape(url)}"
+  end
+
+  def cgi_escape(url)
+    CGI.escape(url).html_safe
   end
 end
