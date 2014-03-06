474cd430db46ebadb2a43c5033639ed4ad62eb71
Steve Ellis <email@steveell.is>
2014-03-06 16:17:12 -0500

    JS to submit feedback with

---
+++ spec/javascripts/walkthrough_feedback_spec.js
@@ -0,0 +1,29 @@
+describe('walkthroughFeedback', function () {
+  var $fixture, $form;
+
+  beforeEach(function () {
+    $fixture = setFixture('<form data-behavior="walkthroughFeedback" data-phone-call-id="1248">1</form>');
+    $form = $fixture.find('form');
+  });
+
+  describe('on success', function () {
+    it('displays the next step', function () {
+      spyOn(jQuery, 'ajax').and.callFake(function (options) {
+        options.success({}, 'textStatus', 'jqXHR');
+      });
+      spyOn(PublicStand, 'displayNextStep');
+
+      $form.submit();
+
+      expect(PublicStand.displayNextStep).toHaveBeenCalled();
+    });
+
+    it('posts to the correct url', function () {
+      spyOn(jQuery, 'ajax').and.callFake(function (options) {
+        expect(options.url).toEqual('/phone_calls/1248/phone_call_feedbacks');
+      });
+
+      $form.submit();
+    });
+  });
+});
