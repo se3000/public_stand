899a14d63f5c0896c115834802a5ff037c9f3885
Steve Ellis <email@steveell.is>
2014-03-13 14:16:44 -0400

    Create lookup methods for different routes

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,4 +5,13 @@ class Organization < ActiveRecord::Base
 
   validates :name, presence: true
   validates :vanity_string, presence: true, uniqueness: true
+
+  def self.lookup(host_url, vanity_string, organization_id)
+    organization = find_by_host_url(host_url) if host_url.present?
+    organization || find_by_vanity_string(vanity_string) || find(organization_id)
+  end
+
+  def campaign_lookup(vanity_string, campaign_id)
+    campaigns.find_by_vanity_string(vanity_string) || campaigns.find_by_id(campaign_id)
+  end
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -19,4 +19,55 @@ describe Organization do
       it { should_not have_valid(:vanity_string).when(other_organization.vanity_string, '', nil)}
     end
   end
+
+  describe ".lookup" do
+    subject{ Organization.lookup(host_url, vanity_string, organization_id) }
+
+    let!(:organization_by_host_url) { FactoryGirl.create(:organization, host_url: 'wooo!') }
+    let!(:organization_by_vanity_string) { FactoryGirl.create(:organization) }
+    let!(:organization_by_id) { FactoryGirl.create(:organization) }
+    let(:host_url) { organization_by_host_url.host_url }
+    let(:vanity_string) { organization_by_vanity_string.vanity_string }
+    let(:organization_id) { organization_by_id.id }
+
+    context "when the host_url is present" do
+      it { should == organization_by_host_url }
+    end
+
+    context "when the vanity string is present but no host_url" do
+      let(:host_url) { nil }
+      it { should == organization_by_vanity_string }
+    end
+
+    context "when just the id is present" do
+      let(:host_url) { nil }
+      let(:vanity_string) { nil }
+      it { should == organization_by_id }
+    end
+  end
+
+  describe "#campaign_lookup" do
+    subject { organization.campaign_lookup(vanity_string, campaign_id) }
+
+    let(:organization) { Organization.first }
+    let(:campaign_by_vanity) { FactoryGirl.create(:campaign, organization: organization) }
+    let(:campaign_by_id) { FactoryGirl.create(:campaign, organization: organization) }
+    let(:vanity_string) { campaign_by_vanity.vanity_string }
+    let(:campaign_id) { campaign_by_id.id }
+
+    context "when the vanity string is present" do
+      it { should == campaign_by_vanity }
+    end
+
+    context "when only the id is present" do
+      let(:vanity_string) { nil }
+      it { should == campaign_by_id }
+    end
+
+    context "when only the id is present" do
+      let(:vanity_string) { nil }
+      let(:campaign_id) { nil }
+      it { should be_nil }
+    end
+  end
 end
